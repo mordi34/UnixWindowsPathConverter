9fca67547984e4d0edc7c23933e9d74c545d1cf9
mordi34 <mserraf34@gmail.com>
2013-05-01 03:47:03 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Mordi Serraf
+ *
+ * Created on May 1, 2013, 3:39 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
